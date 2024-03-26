2d97781235c4ad9ef88bf0d2cb03181d40d530fb
tamanna <tamannaislam1940@gmail.com>
2024-03-26 12:28:28 +0600

    Eommerce-2

---
+++ script/index.js
@@ -0,0 +1,21 @@
+const menuicon=document.getElementById("menu-icon");
+const menu=document.getElementById("menu");
+
+menuicon.addEventListener("click",()=>{
+    if(menu.className=="hidden"){
+menu.classList.remove("hidden");
+    }
+    else{
+        menu.classList.add("hidden")
+    }
+
+
+
+
+
+
+
+
+
+
+})
